eb2a2494f04b3b5c4b92b115ff1e842be87d614d
Hannes Schröder <hannes.schroeder@outlook.com>
2020-05-26 11:08:05 +0200

    init env var

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const CUSTOM_ENV_VAR = process.env.CUSTOM_ENV_VAR;
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -6,7 +8,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
     console.log("Microservices rock!");
-    console.log(`Custom environment variable is: ${customEnvVar}`);
+    console.log(`Custom environment variable is: ${CUSTOM_ENV_VAR}`);
     await sleep(5000);
   }
 }
